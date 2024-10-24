89cd82a515e6c25106420763470c1baf7d918718
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-10-24 09:08:51 -0400

    Update EventMessage structure to be more ezmsg-like

--- src/ezmsg/event/message.py
+++ src/ezmsg/event/message.py
@@ -4,14 +4,14 @@ from numbers import Number
 
 @dataclass
 class EventMessage:
-    timestamp: float
-    """The time at which the event occurred. This is a float in seconds.
-    The clock should usually use time.time() unless otherwise specified."""
+    offset: float
+    """The temporal offset at which the event occurred. This is a float in seconds. The reference point is
+    unspecified and depends on the clock the application uses. Most applications will use time.time."""
 
     ch_idx: int
 
     sub_idx: int = 0
-    """The sub-index of the channel. This is used for multi-unit data, and is usually 0 for single-unit data."""
+    """The sub-index of the channel. For Blackrock multi-unit data: 0=unsorted, 1-5 sorted unit, >5=noise"""
 
     value: Number = 1
     """The value of the event. This can be any number, but is usually an integer, and is often 1 for spikes."""
