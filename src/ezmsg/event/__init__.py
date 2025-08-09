5cbf67ab94fed1913a2258eab21b6060f6005286
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-08-08 23:15:18 -0400

    First commit of BinnedEventAggregator, faster than `Rate`

--- src/ezmsg/event/__init__.py
+++ src/ezmsg/event/__init__.py
@@ -2,3 +2,4 @@ from .__version__ import __version__ as __version__
 
 from .rate import Rate, EventRate
 from .aggregate import Aggregate
+from .binned import BinnedEventAggregator, BinnedEventAggregatorUnit
